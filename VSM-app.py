ac729bbf666ff66e092ea4a9922fee3c9cc52eb3
xadityahx <adityamhegde@icloud.com>
2022-01-20 12:02:43 +0530

    v 3.4

--- VSM-app.py
+++ VSM-app.py
@@ -3,9 +3,14 @@ import os
 import mysql.connector as mysql
 import pandas as pd
 import time
+import sys
 
 clear_console = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 clear_console()
+
+app = Tk()
+app.title('VSM')
+from PIL import Image, ImageTk
 
 with open('config.txt', 'r') as f:
     l = f.readline()
@@ -44,11 +49,13 @@ def chk_tbl(aptname=str):
     else:
         print(aptname)
         choice = input('False\nDo you want to create a new one(y/n) : ')
-        crt_apt(aptname)
+        if choice.lower() == 'y':
+            crt_apt(aptname)
 
 
 # show records of a specific table
 coln = ['Name', 'House No', 'Reason', 'Entry', 'Exit', 'Accreg', 'Status']
+
 
 # Shows the records of a table - people inside and outside
 def show_rec(aptname=str):
@@ -57,20 +64,50 @@ def show_rec(aptname=str):
     cursor.execute(query)
     records = cursor.fetchall()
     for record in records:
-        name, house, reason, entry, ext, accreg, status = record[1], record[2], record[3], record[4], record[5], \
-                                                           record[6], record[7]
+        name, house, reason, entry, ext, accreg, stat = record[1], record[2], record[3], record[4], record[5], \
+                                                        record[6], record[7]
         print(record)
-        if str(status) == "None":
+        if stat == 1:
+            ext = 'Still Inside'
             print("Not inside")
-        df.loc[len(df.index)] = [name, house, reason, entry, ext, accreg, status]
+        df.loc[len(df.index)] = [name, house, reason, entry, ext, accreg, stat]
+
     print(df)
+
+    for wid in app.winfo_children():
+        wid.destroy()
+
+    constnt()
+
+    cols = ['Name', 'House', 'Reason', 'Entry Time', 'Exit Time']
+    for c in range(len(cols)):
+        col = Label(app, text=cols[c])
+        col.grid(row=1, column=c)
+
+    i = 2
+    records = list(records)
+    for record in records:
+        record = record[1:6]
+        record = list(record)
+        for j in range(len(record)):
+            e = Entry(app, width=10, fg='blue')
+            e.grid(row=i, column=j, ipadx=26)
+            if str(record[j]) == 'None':
+                record[j] = 'Still Inside'
+            e.insert(END, record[j])
+        i = i + 1
+
+    def ext_home():
+        HOMESCR(apt=aptname)
+
+    btn = Button(app, text='Exit Home', command=ext_home, width=26).grid(row=i, pady=5, columnspan=len(records[0]))
 
 
 # Add a visitor to a table
 def add_rec(aptname=str, name=str, house=str, reason=str, accreg=bool, status=bool):
     query = "INSERT INTO `%s`.`%s` (`name`, `house`, `reason`, `idate`, `fdate`, `accreg`, `status`) VALUES ('%s',\
      '%s', '%s', NOW(), NULL, %d, %d)" % (
-    l[3], aptname, name, house, reason, accreg, status)
+        l[3], aptname, name, house, reason, accreg, status)
     cursor.execute(query)
     db.commit()
 
@@ -84,10 +121,11 @@ def remove(aptname=str, name=str, house=str):
     record = record[0]
     if record[-1] == 1:
         query_r = "UPDATE `%s`.`%s` SET `fdate` = NOW(), `status` = '0' WHERE name='%s' AND house='%s'" % (
-        l[3], aptname, name, house)
+            l[3], aptname, name, house)
         print('Stage 2')
         cursor.execute(query_r)
         db.commit()
+
 
 # get data of a specific record !!!
 class Visitor:
@@ -107,11 +145,12 @@ class Visitor:
             vname, vhouse, reason, entry, ext, accreg, status = record[1:]
             # print(vname, vhouse, reason, entry, ext, accreg, status)
 
+
 # Function to check who all are inside
 def status_in(aptname=str):
     coln = ['Visitor', 'House', 'Reason', 'Entry']
     df = pd.DataFrame(columns=coln)
-    query_i = "SELECT * FROM %s.%s WHERE `status`=1;"%(l[3], aptname)
+    query_i = "SELECT * FROM %s.%s WHERE `status`=1;" % (l[3], aptname)
     cursor.execute(query_i)
     records = cursor.fetchall()
     for record in records:
@@ -120,11 +159,32 @@ def status_in(aptname=str):
         df.loc[len(df.index)] = [name, house, reason, entry]
     print(df)
 
+    for wid in app.winfo_children():
+        wid.destroy()
 
+    constnt()
 
-app = Tk()
-app.title('VSM')
-from PIL import Image, ImageTk
+    cols = ['Name', 'House', 'Reason', 'Entry Time']
+    for c in range(len(cols)):
+        col = Label(app, text=cols[c])
+        col.grid(row=1, column=c)
+
+    i = 2
+    records = list(records)
+    for record in records:
+        record = record[1:5]
+        record = list(record)
+        for j in range(len(record)):
+            e = Entry(app, width=10, fg='blue')
+            e.grid(row=i, column=j, ipadx=26)
+            e.insert(END, record[j])
+        i = i + 1
+
+    def ext_home():
+        HOMESCR(apt=aptname)
+
+    btn = Button(app, text='Exit Home', command=ext_home, width=26).grid(row=i, pady=5, columnspan=len(records[0]))
+
 
 def constnt():
     # consistent GUI
@@ -137,6 +197,7 @@ def constnt():
     label.image = photo
     label.grid(row=0, column=1)
 
+
 def STARTSRC():
     for i in app.winfo_children():
         i.destroy()
@@ -148,8 +209,7 @@ def STARTSRC():
         str(aptinfo).replace(" ", "")
         chk_tbl(aptname=aptinfo)
         print("Fetching table for", aptinfo)
-        HOMESCRC(apt=aptinfo)
-
+        HOMESCR(apt=aptinfo)
 
     l1 = Label(app, text='Apartment Name :')
     l1.grid(row=1, column=0, padx=5)
@@ -162,9 +222,10 @@ def STARTSRC():
     apt_sel = Button(app, text='Select Apartment!', width=26, command=selected)
     apt_sel.grid(row=2, columnspan=2, pady=5)
 
-
     pass
-def HOMESCRC(apt=str):
+
+
+def HOMESCR(apt=str):
     for i in app.winfo_children():
         i.destroy()
 
@@ -173,12 +234,21 @@ def HOMESCRC(apt=str):
 
     def vis():
         ADDVIS(apt=apt)
+
     def rem():
         REMVIS(apt=apt)
+
     def his():
+        clear_console()  # can comment outr
+        show_rec(aptname=apt)
+        HISTORY(apt=apt)
         pass
+
     def insd():
+        clear_console()  # can comment out
+        VWINSD(apt=apt)
         pass
+
     def change():
         app.title('VMS')
         STARTSRC()
@@ -192,9 +262,9 @@ def HOMESCRC(apt=str):
     ins = Button(app, text='Currently Inside', width=26, command=insd)
     ins.grid(row=5, columnspan=2, pady=1, padx=5)
     chng = Button(app, text='Change Apartment', width=26, command=change)
-    chng.grid(row=5, columnspan=2, pady=1, padx=5)
+    chng.grid(row=6, columnspan=2, pady=1, padx=5)
     qut = Button(app, text='Quit Application', width=26, command=app.destroy)
-    qut.grid(row=6, columnspan=2, pady=1, padx=5)
+    qut.grid(row=7, columnspan=2, pady=1, padx=5)
 
 
 def ADDVIS(apt=str):
@@ -210,7 +280,7 @@ def ADDVIS(apt=str):
         add_rec(aptname=apt, name=name_info, house=house_info, reason=reason_info, accreg=1, status=1)
         print("Record has been added")
         print("Exiting to Home screen")
-        HOMESCRC(apt=apt)
+        HOMESCR(apt=apt)
         pass
 
     nl = Label(app, text='Visitor Name :')
@@ -237,12 +307,13 @@ def ADDVIS(apt=str):
     cr.grid(row=4, column=1)
 
     def back():
-        HOMESCRC(apt=apt)
+        HOMESCR(apt=apt)
 
     addbtn = Button(app, text='Add Visitor', width=26, command=addition)
     addbtn.grid(row=5, columnspan=2, pady=1, padx=5)
     bck = Button(app, text='Back', width=26, command=back)
     bck.grid(row=6, columnspan=2, pady=1, padx=5)
+
 
 def REMVIS(apt=str):
     for i in app.winfo_children():
@@ -270,16 +341,23 @@ def REMVIS(apt=str):
     def rem_visi():
         remove(aptname=apt, name=ne.get(), house=he.get())
         print("Executed")
-        HOMESCRC(apt=apt)
+        HOMESCR(apt=apt)
 
     def back():
-        HOMESCRC(apt=apt)
+        HOMESCR(apt=apt)
 
     bck = Button(app, text='Add Visitor', width=26, command=rem_visi)
     bck.grid(row=5, columnspan=2, pady=1, padx=5)
     bck = Button(app, text='Back', width=26, command=back)
     bck.grid(row=6, columnspan=2, pady=1, padx=5)
 
+
+def HISTORY(apt):
+    show_rec(aptname=apt)
+
+
+def VWINSD(apt):
+    status_in(aptname=apt)
 
 
 STARTSRC()
